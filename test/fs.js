77e1609b9a7166e1ff28c4a91f0ff8839f95762d
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2017-08-09 21:02:04 +0200

    Move fs module to separate package

--- test/fs.js
+++ test/fs.js
@@ -1,15 +1,1 @@
-var spec = require('samizdat-spec')
-var rm = require('rimraf')
-
-rm.sync('/tmp/samizdat-fs-basic')
-rm.sync('/tmp/samizdat-fs-stream')
-
-spec.basic('filesystem', {
-    tape: require('tape'),
-    db: require('../fs')('/tmp/samizdat-fs-basic')
-})
-
-spec.stream('filesystem', {
-    tape: require('tape'),
-    db: require('../fs')('/tmp/samizdat-fs-stream')
-})
+require('samizdat-fs/test')
